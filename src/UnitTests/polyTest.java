b4d8f417c66e45ec01a8cad2c5bf230d86180e12
TomerAF <tomeraf@post.bgu.ac.il>
2023-06-01 16:21:09 +0300

    poly tests complete

--- src/UnitTests/polyTest.java
+++ src/UnitTests/polyTest.java
@@ -4,40 +4,40 @@ import calculator.*;
 import org.junit.Assert;
 import org.junit.Test;
 public class polyTest {
-    private Polynomial polynomialA = Polynomial.build("1 -2 3");
-    private Polynomial polynomialB = Polynomial.build("0 1 2 3");
-    private Polynomial polynomialC = Polynomial.build("3 2/3");
-    private Polynomial polynomialD = Polynomial.build("-4 3");
+    private Polynomial poly1 = Polynomial.build("2 1 -4");
+    private Polynomial poly2 = Polynomial.build("0 0 2 2");
+    private Polynomial poly3 = Polynomial.build("1/2 4/3");
+    private Polynomial poly4 = Polynomial.build("2 -1");
     @Test
     public void build() {
-        Assert.assertEquals("expected 1 -2x +3x^2", Polynomial.build("1 -2 3"), polynomialA);
+        Assert.assertEquals("expected 2-x-4x^2", Polynomial.build("2 1 -4"), poly1);
     }
     @Test
-    public void add1() {
-        Assert.assertEquals("expected 1 - x + 5x^2 + 3x^3", Polynomial.build("1 -1 5 3"), polynomialA.add(polynomialB));
+    public void add12() {
+        Assert.assertEquals("expected 2+x-2x^2+2x^3", Polynomial.build("2 1 -2 2"), poly1.add(poly2));
     }
-    public void add2() {
-        Assert.assertEquals("expected 4 - 4/3x + 3", Polynomial.build("4 -4/3 3"), polynomialA.add(polynomialC));
-    }
-    @Test
-    public void mul() {
-        Assert.assertEquals("- 12 + 19/3 + 2", Polynomial.build("-12 19/3 2/1"), polynomialC.mul(polynomialD));
+    public void add13() {
+        Assert.assertEquals("expected 5/2+7/3x-4x^2", Polynomial.build("5/2 7/3 -4"), poly1.add(poly3));
     }
     @Test
-    public void evaluateInteger() {
-        Assert.assertEquals("expected 9", new Integer(9), polynomialA.evaluate(new Integer(2)));
+    public void mul34() {
+        Assert.assertEquals("expected -1+19/6x-4/3x^2", Polynomial.build("-1 19/6 -4/3"), poly3.mul(poly4));
     }
     @Test
-    public void evaluateRational() {
-        Assert.assertEquals("expected 11/8", new Rational(3,4), polynomialA.evaluate(new Rational(1,2)));
+    public void evaluateInteger1() {
+        Assert.assertEquals("expected 20", new Integer(20), poly1.evaluate(new Integer(2)));
+    }
+    @Test
+    public void evaluateRational1() {
+        Assert.assertEquals("expected 7/2", new Rational(7,2), poly1.evaluate(new Rational(1,2)));
 
     }
     @Test
-    public void derivative() {
-        Assert.assertEquals("expected 1 + 4 + 9", Polynomial.build("1 4 9"), polynomialB.derivative());
+    public void derivative2() {
+        Assert.assertEquals("expected 1-4x", Polynomial.build("1 -4"), poly2.derivative());
     }
     @Test
-    public void testEquals() {
-        Assert.assertEquals("expected true", true, polynomialA.equals(Polynomial.build("1 -4/2 3")));
+    public void testEquals1() {
+        Assert.assertEquals("expected true", true, poly1.equals(Polynomial.build("1 -4/2 3")));
     }
 }
